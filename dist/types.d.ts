a215e51b3226729fbf132f935849340adc5b6093
Joseph Cheng <the_only_light@hotmail.com>
2023-05-09 15:05:45 +0800

    Fix/format (#6)
    * Run prettier
    
    * Bump to 1.2.1

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,20 +1,20 @@
 export type DefaultConfig = {
-    verbose?: boolean;
+  verbose?: boolean;
 };
 export type RunConfig = {
-    job: () => void;
-    jobName: string;
-    immediate?: boolean;
-    immediateCallback?: () => void;
-    interval: string;
-    delay?: boolean;
-    delayDuration?: number;
-    /**
-     * Example: Asia/Singapore, Pacific/Honolulu
-     */
-    timezone?: string;
-    onError?: () => void;
+  job: () => void;
+  jobName: string;
+  immediate?: boolean;
+  immediateCallback?: () => void;
+  interval: string;
+  delay?: boolean;
+  delayDuration?: number;
+  /**
+   * Example: Asia/Singapore, Pacific/Honolulu
+   */
+  timezone?: string;
+  onError?: () => void;
 };
 export type StopRunConfig = {
-    jobName: string;
+  jobName: string;
 };
